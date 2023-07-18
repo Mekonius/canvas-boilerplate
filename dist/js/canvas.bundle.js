7137bbe1c2f15d797b4749d750fbca48208de90c
Kenneth Løvgren <kenneth.l.thomsen@gamil.com>
2023-07-18 23:37:23 +0200

    made a pretty uniqe form

--- dist/js/canvas.bundle.js
+++ dist/js/canvas.bundle.js
@@ -97,12 +97,12 @@
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _utils__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./utils */ "./src/js/utils.js");
 /* harmony import */ var _utils__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_utils__WEBPACK_IMPORTED_MODULE_0__);
+function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, _toPropertyKey(descriptor.key), descriptor); } }
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
+function _toPropertyKey(arg) { var key = _toPrimitive(arg, "string"); return _typeof(key) === "symbol" ? key : String(key); }
+function _toPrimitive(input, hint) { if (_typeof(input) !== "object" || input === null) return input; var prim = input[Symbol.toPrimitive]; if (prim !== undefined) { var res = prim.call(input, hint || "default"); if (_typeof(res) !== "object") return res; throw new TypeError("@@toPrimitive must return a primitive value."); } return (hint === "string" ? String : Number)(input); }
 
 var canvas = document.querySelector('canvas');
 var c = canvas.getContext('2d');
@@ -112,8 +112,9 @@ var mouse = {
   x: innerWidth / 2,
   y: innerHeight / 2
 };
-var colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']; // Event Listeners
+var colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66'];
 
+// Event Listeners
 addEventListener('mousemove', function (event) {
   mouse.x = event.clientX;
   mouse.y = event.clientY;
@@ -121,57 +122,72 @@ addEventListener('mousemove', function (event) {
 addEventListener('resize', function () {
   canvas.width = innerWidth;
   canvas.height = innerHeight;
-  init();
-}); // Objects
+});
 
-var _Object = /*#__PURE__*/function () {
-  function Object(x, y, radius, color) {
-    _classCallCheck(this, Object);
-
+// Objects
+var Particle = /*#__PURE__*/function () {
+  function Particle(x, y, radius, color) {
+    _classCallCheck(this, Particle);
     this.x = x;
     this.y = y;
     this.radius = radius;
     this.color = color;
+    this.radians = Math.random() * Math.PI * 2;
+    this.velocity = 0.05;
+    //size of the circle
+    this.chaos = Object(_utils__WEBPACK_IMPORTED_MODULE_0__["randomIntFromRange"])(50, 120);
   }
-
-  _createClass(Object, [{
+  _createClass(Particle, [{
     key: "draw",
-    value: function draw() {
+    value: function draw(oldPos) {
       c.beginPath();
-      c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-      c.fillStyle = this.color;
-      c.fill();
+      c.strokeStyle = this.color;
+      c.lineWidth = this.radius;
+      c.save();
+      c.moveTo(oldPos.x, oldPos.y);
+      c.lineTo(this.x, this.y);
+      c.stroke();
       c.closePath();
+      c.shadowColor = this.color;
+      c.shadowBlur = 100;
+      c.restore();
     }
   }, {
     key: "update",
     value: function update() {
-      this.draw();
+      var oldPos = {
+        x: this.x,
+        y: this.y
+      };
+      this.radians += this.velocity;
+      this.x = canvas.width / 2 + Math.cos(this.radians) * this.chaos * 0 + Math.cos(this.radians) * this.chaos * Math.sin(-this.radians) * 2.1;
+      this.y = canvas.height / 2 + Math.sin(this.radians) * this.chaos * 3 + Math.sin(this.radians) / this.chaos * Math.cos(-this.radians) * 2.1;
+      this.draw(oldPos);
     }
   }]);
-
-  return Object;
+  return Particle;
 }(); // Implementation
-
-
-var objects;
-
+var particles;
 function init() {
-  objects = [];
-
-  for (var i = 0; i < 400; i++) {// objects.push()
+  particles = [];
+  for (var i = 0; i < 50; i++) {
+    var radius = Math.random() * 2;
+    particles.push(new Particle(canvas.width / 2, canvas.height / 2, radius * 2, Object(_utils__WEBPACK_IMPORTED_MODULE_0__["randomColor"])(colors)));
   }
-} // Animation Loop
-
-
-function animate() {
-  requestAnimationFrame(animate);
-  c.clearRect(0, 0, canvas.width, canvas.height);
-  c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y); // objects.forEach(object => {
-  //  object.update()
-  // })
+  console.log(particles);
+  canvas.style.backgroundColor = 'black';
 }
 
+// Animation Loop
+function animate() {
+  requestAnimationFrame(animate);
+  //c.clearRect(0, 0, canvas.width, canvas.height)
+  c.fillStyle = 'rgba(0, 0, 0, 0.05)';
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  particles.forEach(function (p) {
+    p.update();
+  });
+}
 init();
 animate();
 
@@ -187,17 +203,14 @@ animate();
 function randomIntFromRange(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
-
 function randomColor(colors) {
   return colors[Math.floor(Math.random() * colors.length)];
 }
-
 function distance(x1, y1, x2, y2) {
   var xDist = x2 - x1;
   var yDist = y2 - y1;
   return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
 }
-
 module.exports = {
   randomIntFromRange: randomIntFromRange,
   randomColor: randomColor,
--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,4 +1,4 @@
-import utils from './utils'
+import utils, { randomColor, randomIntFromRange } from './utils'
 
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
@@ -22,51 +22,70 @@ addEventListener('mousemove', (event) => {
 addEventListener('resize', () => {
   canvas.width = innerWidth
   canvas.height = innerHeight
-
-  init()
 })
 
 // Objects
-class Object {
+class Particle {
   constructor(x, y, radius, color) {
     this.x = x
     this.y = y
     this.radius = radius
     this.color = color
+    this.radians = Math.random() * Math.PI * 2;
+    this.velocity = 0.05
+    //size of the circle
+    this.chaos = randomIntFromRange(50, 120);
   }
 
-  draw() {
+  draw(oldPos) {
     c.beginPath()
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    c.fillStyle = this.color
-    c.fill()
+    c.strokeStyle = this.color
+    c.lineWidth = this.radius
+
+    c.save()
+    c.moveTo(oldPos.x, oldPos.y)
+    c.lineTo(this.x, this.y)
+    c.stroke()
     c.closePath()
+    c.shadowColor = this.color;
+    c.shadowBlur = 100;
+
+    c.restore()
   }
 
   update() {
-    this.draw()
+    const oldPos = {x: this.x, y: this.y};
+    this.radians += this.velocity
+    this.x = canvas.width / 2 + Math.cos(this.radians) * 
+      this.chaos * 0 + Math.cos(this.radians) * this.chaos * Math.sin(-this.radians) * 2.1;
+    this.y = canvas.height / 2  + Math.sin(this.radians) *
+      this.chaos * 3 + Math.sin(this.radians) / this.chaos * Math.cos(-this.radians) * 2.1;
+    this.draw(oldPos)
   }
 }
 
 // Implementation
-let objects
+let particles
 function init() {
-  objects = []
+  particles = []
 
-  for (let i = 0; i < 400; i++) {
-    // objects.push()
+  for (let i = 0; i < 50; i++) {
+    const radius = (Math.random() * 2);
+    particles.push(new Particle(canvas.width / 2, canvas.height /2, radius * 2, randomColor(colors)));
   }
+  console.log(particles);
+  canvas.style.backgroundColor = 'black';
 }
 
 // Animation Loop
 function animate() {
   requestAnimationFrame(animate)
-  c.clearRect(0, 0, canvas.width, canvas.height)
-
-  c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y)
-  // objects.forEach(object => {
-  //  object.update()
-  // })
+  //c.clearRect(0, 0, canvas.width, canvas.height)
+  c.fillStyle = 'rgba(0, 0, 0, 0.05)'
+  c.fillRect(0, 0, canvas.width, canvas.height)
+   particles.forEach(p => {
+    p.update()
+  })
 }
 
 init()
